8115e1cc87e6df483004ba29617ca1151abc0f3c
B-G-H <barbara.henn@huntheim.de>
2021-06-17 14:31:30 +0200

    gg

---
+++ mein.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="mein" tilewidth="32" tileheight="32" tilecount="667" columns="29">
+ <image source="../../../Downloads/tiled-master/tiled-master/examples/rpg/island.tmx" width="928" height="752"/>
+</tileset>
